b4c4b4adfb842436549187de883f3dc0ad71f709
Alexandr <schurik1983@bk.ru>
2023-11-21 15:08:35 +0300

    saving the third task

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadInt(string msg)
+{
+    while(true)
+    {
+    System.Console.WriteLine(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number < 0)
+    {
+        System.Console.WriteLine("Число должно быть больше или равно 0 !");
+        continue;
+    }
+    return number;
+    }
+}
+int formulaAck(int m, int n)
+{   if(m == 0) return n + 1;
+    else if(n == 0) return formulaAck(m - 1, 1);
+    else return formulaAck(m - 1, formulaAck(m, n - 1));
+}
+
+int m = ReadInt("Введите первое число");
+int n = ReadInt("Введите второе число");
+Console.WriteLine(formulaAck(m, n));
